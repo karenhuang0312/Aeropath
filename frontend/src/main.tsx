9f3f44a42e198da9ba85523b39e7f3d448599da5
Karen Huang <karenhuang0312@gmail.com>
2025-08-03 15:43:41 -0400

    Update main.tsx

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,10 +1,17 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App.tsx';
+import './index.css';
+
+import { LanguageProvider } from './context/LanguageProvider';
+import { AuthProvider } from './hooks/useAuth'; // Only if you're not already wrapping this in App.tsx
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
+    <LanguageProvider>
+      <AuthProvider>
+        <App />
+      </AuthProvider>
+    </LanguageProvider>
   </React.StrictMode>,
-)
+);
