b6cef84b8458cb5e05c43764b200f3572d5f5f2a
Karen Huang <karenhuang0312@gmail.com>
2025-08-04 19:31:26 -0400

    Update Home.tsx

--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { useNavigate } from 'react-router-dom';
-import { DateAndTimePicker } from '../components/DateAndTimePicker';
+import DateAndTimePicker from '../components/DateAndTimePicker';
 
 const Home = () => {
   const navigate = useNavigate();
